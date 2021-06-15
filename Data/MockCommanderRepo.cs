4b55ed28ae4b6616c3fcf19172f8d98bcc92a390
Henry Li <henry226@bu.edu>
2021-06-14 22:05:03 -0400

    Adding Data folder

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commends = new List<Command>
+            {
+                new Command { Id = 0, HowTo = "How To 0", Line = "Line 0", Platform = "Platform 0" },
+                new Command { Id = 1, HowTo = "How To 0", Line = "Line 0", Platform = "Platform 0" },
+                new Command { Id = 2, HowTo = "How To 0", Line = "Line 0", Platform = "Platform 0" }
+            };
+            
+            return commends;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "How To", Line = "Line", Platform = "Platform"};
+        }
+    }
+}
